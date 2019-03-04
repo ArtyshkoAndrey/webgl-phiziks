ad7cfdc425c92fe196c8ea08431f73b96de71fe3
Fullion <artyshko.andrey@gmail.com>
2019-03-04 21:17:03 +0700

    added new material and sphere

--- init.js
+++ init.js
@@ -1,5 +1,5 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 0.1, 3000 );
 var controls = new THREE.OrbitControls( camera );
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -10,12 +10,32 @@ scene.add( ambLight );
 var light = new THREE.DirectionalLight( 0xdddddd, 1 );
 light.position.set( -100, 100, 100 );
 scene.add( light );
+
 var geometry = new THREE.SphereGeometry( 60, 24, 16 );
 var material = new THREE.MeshPhongMaterial({ color: 0xaaaaaa, shininess: 40 });
 var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
-camera.position.set(100, 0, 1000)
+var geometry2 = new THREE.SphereGeometry( 60, 24, 16 );
+var material2 = new THREE.MeshPhongMaterial({ color: 0xaaaaaa, shininess: 40 });
+var cube2 = new THREE.Mesh( geometry, material );
+cube2.position.x = 200
+
+
+
+let line = new Array(100/20)
+
+for (let i = 1; i < line.length + 1; i++) {
+	line[i - 1] = {}
+	line[i - 1].geometri = new THREE.BoxGeometry( 20, 20, 20 )
+	line[i - 1].material = new THREE.MeshNormalMaterial();
+	line[i - 1].mesh = new THREE.Mesh(line[i - 1].geometri, line[i - 1].material)
+	line[i - 1].mesh.position.x = 40 + i * 20
+	scene.add(line[i - 1].mesh)
+}
+scene.add( cube, cube2 );
+
+
+camera.position.set(200, 300, 1000)
 
 var animate = function () {
   requestAnimationFrame( animate );
