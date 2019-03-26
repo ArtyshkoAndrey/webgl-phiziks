5734287a1adfde24ac2baa6971f33e1665ed4fc7
Fullion <artyshko.andrey@gmail.com>
2019-03-26 17:26:11 +0700

    Убрана лишняя проевка, Оптимизация кода

--- static/Graphics.js
+++ static/Graphics.js
@@ -82,23 +82,10 @@ export default class Graphics {
     vector.set((event.offsetX / this.canvas.width) * 2 - 1, -(event.offsetY / this.canvas.height) * 2 + 1, 0.05) // z = 0.5 important!
     vector.unproject(this.camera)
     raycaster.set(this.camera.position, vector.sub(this.camera.position).normalize())
-    let intersects = raycaster.intersectObjects(this.scene.children, true)
+    let intersects = raycaster.intersectObjects(this.ObjectMolecule.children, true)
     if (intersects.length > 0) {
-      console.log(intersects)
-      if (intersects[0].object.type !== 'LineSegments') {
-        if (intersects[0].object.geometry instanceof THREE.SphereGeometry) {
-          this.molecule.tick(intersects[0])
-          // console.log(intersects[0].object)
-        }
-      } else {
-        if (typeof intersects[3] === 'object') {
-          if (intersects[3].object instanceof THREE.Mesh) {
-            if (intersects[3].object.geometry instanceof THREE.SphereGeometry) {
-              this.molecule.tick(intersects[3])
-              // console.log(intersects[1].object)
-            }
-          }
-        }
+      if (intersects[0].object.geometry instanceof THREE.SphereGeometry) {
+        this.molecule.tick(intersects[0])
       }
     }
     this.renderer.render(this.scene, this.camera)
@@ -126,37 +113,15 @@ export default class Graphics {
     vector.set(((event.clientX - 150) / this.canvas.width) * 2 - 1, -(event.clientY / this.canvas.height) * 2 + 1, 0.05) // z = 0.5 important!
     vector.unproject(this.camera)
     raycaster.set(this.camera.position, vector.sub(this.camera.position).normalize())
-    let intersects = raycaster.intersectObjects(this.scene.children, true)
+    let intersects = raycaster.intersectObjects(this.ObjectMolecule.children, true)
     if (intersects.length > 0) {
-      if (intersects[0].object.type !== 'LineSegments') {
-        if (intersects[0].object.geometry instanceof THREE.SphereGeometry) {
-          // console.log(intersects[0].object)
-          div.style.top = event.clientY + 10 + 'px'
-          div.style.left = event.clientX + 10 + 'px'
-          div.style.display = 'block'
-          div.textContent = intersects[0].object.userData['AtomName']
-        } else {
-          div.style.display = 'none'
-        }
+      if (intersects[0].object.geometry instanceof THREE.SphereGeometry) {
+        div.style.top = event.clientY + 10 + 'px'
+        div.style.left = event.clientX + 10 + 'px'
+        div.style.display = 'block'
+        div.textContent = intersects[0].object.userData['AtomName']
       } else {
-        // console.log(typeof intersects[1])
-        if (typeof intersects[3] === 'object') {
-          if (intersects[3].object instanceof THREE.Mesh) {
-            if (intersects[3].object.geometry instanceof THREE.SphereGeometry) {
-              // console.log(intersects[1].object)
-              div.style.top = event.clientY + 10 + 'px'
-              div.style.left = event.clientX + 10 + 'px'
-              div.style.display = 'block'
-              div.textContent = intersects[3].object.userData['AtomName']
-            } else {
-              div.style.display = 'none'
-            }
-          } else {
-            div.style.display = 'none'
-          }
-        } else {
-          div.style.display = 'none'
-        }
+        div.style.display = 'none'
       }
     } else {
       div.style.display = 'none'
