f666281ad2b7617264f6471e4d276c5db600deac
Fullion <artyshko.andrey@gmail.com>
2019-05-06 16:19:11 +0700

    Update graphics.vue

--- src/renderer/components/graphics.vue
+++ src/renderer/components/graphics.vue
@@ -1,5 +1,35 @@
 <template>
   <v-container fluid style="padding: 0; position: initial;">
+    <v-layout row wrap style="width: 150px; z-index: 10">
+      <v-flex md12 xs12 class="px-2">
+        <div id="InfoForAtom">
+        </div>
+      </v-flex>
+      <v-flex xs12 class="px-2">
+        <v-btn color="primary" block>Drag</v-btn>
+<!--        <v-btn color="primary" block @click="drag" v-else>Tick</v-btn>-->
+      </v-flex>
+      <!-- Создание атома -->
+      <v-flex xs12 class="px-2 py-3">
+        <h4>Add new Atom</h4>
+        <v-select class="py-0"
+                  v-model='newAtom'
+                  :items="Object.entries($parent.$parent.$parent.colorAtoms.atoms)"
+                  item-text="[1].name"
+                  item-value="[1].symbol"
+                  single-line
+                  return-object
+                  persistent-hint
+                  label="Atom name"
+        ></v-select>
+        <v-btn color="success" block>Добавить</v-btn>
+      </v-flex>
+      <v-flex class="px-2 py-3">
+        <!-- Кнопка удаления -->
+        <h4>Delete selected Atom</h4>
+        <v-btn color="error" block >Удалить</v-btn>
+      </v-flex>
+    </v-layout>
     <canvas id="gl" style='z-index: 1'></canvas>
   </v-container>
 </template>
@@ -44,16 +74,14 @@
         return document.getElementById('gl')
       }
     },
-    methods: {
-      goToHome  () {
-        this.gl.destructor()
-        this.molecule.destructor()
-        this.gl = null
-        this.molecule = null
-        delete this.gl
-        delete this.molecule
-        this.$router.push('index')
-      }
+    beforeRouteLeave (to, from, next) {
+      this.gl.engine.dispose()
+      this.gl.scene.dispose()
+      this.gl.molecule._children.forEach(atom => {
+        atom.material.dispose()
+        atom.dispose()
+      })
+      next()
     }
   }
 </script>
